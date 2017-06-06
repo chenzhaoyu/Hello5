c19c68c520c71e95d47e3bbabca1eace56df4e46
chenzhaoyu <chenzhaoyu@navercorp.com>
2017-06-06 16:50:49 +0800

    add spring

---
+++ src/main/java/com/naver/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.naver;
+
+public class HelloWorld {
+    private String info;
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+
+}
--- src/test/java/com/naver/BeanTest.java
+++ src/test/java/com/naver/BeanTest.java
@@ -1,23 +1,20 @@
 package com.naver;
 
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class BeanTest
+        extends TestCase
 {
     private String who = null;
-
-    public static void main(String[] args) {
-        //获取Spring的ApplicationContext配置文件，注入IOC容器中
-        //(Map: key:String, bean标签的id属性值 ==>value:Object, bean标签class属性所指类的实例)
+    public void testBean() {
         BeanFactory factory = new ClassPathXmlApplicationContext("applicationContext.xml");
         HelloWorld hw1 = (HelloWorld)factory.getBean("hello");//map.get("hello")
-        System.out.println(hw1.getInfo());
-        System.out.println(hw1);
+        String ww = hw1.getInfo();
+        assertTrue(ww.equals("Happy New Year!"));
 
     }
 }
